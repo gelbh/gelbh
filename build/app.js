d4bbff0ce3367cfabe5140ab188bf60b9cefbb11
gelbh <gelbharttomer@gmail.com>
2023-12-20 15:26:58 +0000

    docs: auto-update README.md

---
+++ build/app.js
@@ -0,0 +1,95 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs = __importStar(require("fs"));
+const md = require("markdown-it")({
+    html: true, // Enable HTML tags in source
+    breaks: true, // Convert '\n' in paragraphs into <br>
+    linkify: true, // Autoconvert URL-like text to links
+});
+const fetchGitHubData_1 = require("./fetchGitHubData");
+const ossProjectRepos = ["gelbh"];
+const githubUsername = "gelbh";
+const websiteUrl = "https://gelbhart.com";
+const linkedinUrl = "https://linkedin.com/in/tomer-gelbhart";
+function generateMarkdown() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const websiteBadge = `[![Website Badge](https://img.shields.io/badge/-Website-3B7EBF?style=for-the-badge&logo=amp&logoColor=white)](${websiteUrl})`;
+        const linkedinBadge = `[![Linkedin Badge](https://img.shields.io/badge/-LinkedIn-3B7EBF?style=for-the-badge&logo=Linkedin&logoColor=white)](${linkedinUrl})`;
+        const profileCountBadge = `![Profile Views Count Badge](https://komarev.com/ghpvc/?username=${githubUsername}&style=for-the-badge)`;
+        const githubStatsCardDark = `[![GitHub-Stats-Card-Dark](https://github-readme-stats.vercel.app/api?username=${githubUsername}&show_icons=true&hide_border=true&include_all_commits=true&card_width=600&custom_title=GitHub%20Open%20Source%20Stats&title_color=3B7EBF&text_color=FFF&icon_color=3B7EBF&hide=contribs&show=reviews,prs_merged,prs_merged_percentage&theme=transparent#gh-dark-mode-only)](https://github.com/${githubUsername}/${githubUsername}#gh-dark-mode-only)`;
+        const githubStatsCardLight = `[![GitHub-Stats-Card-Light](https://github-readme-stats.vercel.app/api?username=${githubUsername}&show_icons=true&hide_border=true&include_all_commits=true&card_width=600&custom_title=GitHub%20Open%20Source%20Stats&title_color=3B7EBF&text_color=474A4E&icon_color=3B7EBF&hide=contribs&show=reviews,prs_merged,prs_merged_percentage&theme=transparent#gh-light-mode-only)](https://github.com/${githubUsername}/${githubUsername}#gh-light-mode-only)`;
+        const markdownText = `<div align="center">\n
+
+  ${websiteBadge} ${linkedinBadge} ${profileCountBadge}\n
+
+  ---\n
+
+  Hi there 👋🏾!\n
+
+  ---\n
+
+  ${githubStatsCardDark}\n
+  ${githubStatsCardLight}\n
+
+  </div>\n
+
+  ---\n
+
+  ## Highlights
+
+  <details>\n
+  <summary>Projects</summary>\n
+  <br />
+  Here are some of my other projects you might want to check out that are not pinned:\n
+  <br />\n<br />
+  ${yield (0, fetchGitHubData_1.fetchGitHubData)(ossProjectRepos)}\n
+  </details>\n
+
+  ---\n
+
+  <a href="#">Learn how this works.</a> <a href="https://github.com/gelbh/gelbh/actions/workflows/build.yml"><img src="https://github.com/gelbh/gelbh/actions/workflows/build.yml/badge.svg" align="right" alt="Rebuild README.md file"></a>\n
+
+  <div align="center">\n
+   <a href="https://gelbhart.com" target="_blank" rel="noopener noreferrer"></a>\n
+  </div>`;
+        const result = md.render(markdownText);
+        fs.writeFile("README.md", result, (error) => {
+            if (error)
+                throw new Error(`Something went wrong: ${error}.`);
+            console.log(`✅ README.md file was succesfully generated.`);
+        });
+    });
+}
+generateMarkdown();
