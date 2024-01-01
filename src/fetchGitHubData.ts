959e17827bc71b5f929a4a6f35bcf731a36cf08d
Tomer <gelbharttomer@gmail.com>
2024-01-01 10:48:32 +0100

    only show stars and forks count when there are more than 0

--- src/fetchGitHubData.ts
+++ src/fetchGitHubData.ts
@@ -3,9 +3,13 @@ export async function fetchGitHubData(repos: Array<string>): Promise<string> {
 
   const list = await Promise.all(
     repos.map(async (repo) => {
-      const response = await fetch(`https://api.github.com/repos/${owner}/${repo}`);
+      const response = await fetch(
+        `https://api.github.com/repos/${owner}/${repo}`
+      );
       if (!response.ok) {
-        throw new Error(`"${owner}/${repo}" not found. Kindy review your list of repositories.`);
+        throw new Error(
+          `"${owner}/${repo}" not found. Kindly review your list of repositories.`
+        );
       }
       const data = await response.json();
 
@@ -14,10 +18,23 @@ export async function fetchGitHubData(repos: Array<string>): Promise<string> {
         full_name: name,
         stargazers_count: stars,
         forks_count: forks,
-        description: desc
+        description: desc,
       } = data;
 
-      return `<li><a href=${url} target="_blank" rel="noopener noreferrer">${name}</a> (<b>${stars}</b> ✨ and <b>${forks}</b> 🍴): ${desc}</li>`;
+      let starsAndForksDisplay = "";
+      if (stars > 0) {
+        starsAndForksDisplay += `<b>${stars}</b> ✨`;
+      }
+      if (forks > 0) {
+        starsAndForksDisplay += starsAndForksDisplay
+          ? ` and <b>${forks}</b> 🍴`
+          : `<b>${forks}</b> 🍴`;
+      }
+      starsAndForksDisplay = starsAndForksDisplay
+        ? ` (${starsAndForksDisplay})`
+        : "";
+
+      return `<li><a href=${url} target="_blank" rel="noopener noreferrer">${name}</a>${starsAndForksDisplay}: ${desc}</li>`;
     })
   );
 
