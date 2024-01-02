5cf6e2ba32d7cebaf095eeac925205748fa9a76b
Tomer <gelbharttomer@gmail.com>
2024-01-02 19:53:02 +0100

    fix(favicon)

--- src/app.ts
+++ src/app.ts
@@ -57,7 +57,7 @@ async function generateMarkdown() {
   <a href="https://blog.bolajiayodeji.com/how-to-create-an-automated-profile-readme-using-nodejs-and-github-actions?utm_source=github-profile">Learn how this works.</a> <a href="https://github.com/gelbh/gelbh/actions/workflows/build.yml"><img src="https://github.com/gelbh/gelbh/actions/workflows/build.yml/badge.svg" align="right" alt="Rebuild README.md file"></a>\n
 
   <div align="center">\n
-   <a href="https://gelbhart.com" target="_blank" rel="noopener noreferrer"><img src="https://gelbhart.com/logo.png" width="30" /></a>\n
+   <a href="https://gelbhart.com" target="_blank" rel="noopener noreferrer"><img src="https://gelbhart.com/favicon.png" width="30" /></a>\n
   </div>`;
 
   const result = md.render(markdownText);
