bdedcfe6752c3ba299e8c071615af113aac2cb44
Tomer <gelbharttomer@gmail.com>
2024-12-05 17:50:21 +0100

    docs: enhance README layout and update badge generation for improved readability

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 <div align="center">
-  [![Website Badge](https://img.shields.io/badge/-Website-3B7EBF?style=for-the-badge&logo=amp&logoColor=white)](https://gelbhart.dev) [![LinkedIn Badge](https://img.shields.io/badge/-LinkedIn-3B7EBF?style=for-the-badge&logo=Linkedin&logoColor=white)](https://linkedin.com/in/tomer-gelbhart) ![Profile Views](https://komarev.com/ghpvc/?username=gelbh&style=for-the-badge)
-  ---
-  Hi there 👋!
-  ---
-  [![GitHub-Stats-Card-Dark](https://github-readme-stats.vercel.app/api?username=gelbh&show_icons=true&hide_border=true&include_all_commits=true&card_width=600&custom_title=GitHub+Open+Source+Stats&title_color=3B7EBF&text_color=FFF&icon_color=3B7EBF&hide=contribs&show=reviews%2Cprs_merged%2Cprs_merged_percentage&theme=transparent#gh-dark-mode-only)](https://github.com/gelbh/gelbh#gh-dark-mode-only)
-[![GitHub-Stats-Card-Light](https://github-readme-stats.vercel.app/api?username=gelbh&show_icons=true&hide_border=true&include_all_commits=true&card_width=600&custom_title=GitHub+Open+Source+Stats&title_color=3B7EBF&text_color=474A4E&icon_color=3B7EBF&hide=contribs&show=reviews%2Cprs_merged%2Cprs_merged_percentage&theme=transparent#gh-light-mode-only)](https://github.com/gelbh/gelbh#gh-light-mode-only)
+<p><a href="https://gelbhart.dev"><img src="https://img.shields.io/badge/-Website-3B7EBF?style=for-the-badge&logo=amp&logoColor=white" alt="Website Badge"/></a> <a href="https://linkedin.com/in/tomer-gelbhart"><img src="https://img.shields.io/badge/-LinkedIn-3B7EBF?style=for-the-badge&logo=Linkedin&logoColor=white" alt="LinkedIn Badge"/></a> <img src="https://komarev.com/ghpvc/?username=gelbh&style=for-the-badge" alt="Profile Views Count Badge"/></p>
+<hr>
+<p>Hi there 👋!</p>
+<hr>
+<a href="https://github.com/gelbh/gelbh#gh-dark-mode-only">
+      <img src="https://github-readme-stats.vercel.app/api?username=gelbh&show_icons=true&hide_border=true&include_all_commits=true&card_width=600&custom_title=GitHub%20Open%20Source%20Stats&title_color=3B7EBF&text_color=FFF&icon_color=3B7EBF&hide=contribs&show=reviews,prs_merged,prs_merged_percentage&theme=transparent#gh-dark-mode-only" alt="GitHub Stats Dark Mode"/>
+    </a>
+<a href="https://github.com/gelbh/gelbh#gh-light-mode-only">
+      <img src="https://github-readme-stats.vercel.app/api?username=gelbh&show_icons=true&hide_border=true&include_all_commits=true&card_width=600&custom_title=GitHub%20Open%20Source%20Stats&title_color=3B7EBF&text_color=474A4E&icon_color=3B7EBF&hide=contribs&show=reviews,prs_merged,prs_merged_percentage&theme=transparent#gh-light-mode-only" alt="GitHub Stats Light Mode"/>
+    </a>
 </div>
 <hr>
 <h2>Open Source Projects</h2>
@@ -17,6 +21,8 @@
 <li><a href=https://github.com/gelbh/gelbhart-innovations target="_blank" rel="noopener noreferrer">
           gelbh/gelbhart-innovations</a>: This repository powers gelbhart.com, a website crafted with Ruby on Rails. It combines CSS, JavaScript, and HTML for a sleek, user-friendly interface. </li></ul>
 <hr>
+<div align="center">
 <a href="https://gelbhart.dev" target="_blank" rel="noopener noreferrer">
-  <img src="https://gelbhart.dev/favicon.ico" width="30" align="center" />
+  <img src="https://gelbhart.dev/favicon.ico" width="30" alt="Website Icon"/>
 </a>
+</div>
--- src/markdown.service.ts
+++ src/markdown.service.ts
@@ -1,3 +1,4 @@
+// markdown.service.ts
 import MarkdownIt from "markdown-it";
 import { config } from "./config";
 
@@ -13,70 +14,38 @@ export class MarkdownService {
   }
 
   private generateBadges(): string {
-    const badges = [
-      this.createBadge("Website", config.social.website),
-      this.createBadge("LinkedIn", config.social.linkedin, "Linkedin"),
-      `![Profile Views](https://komarev.com/ghpvc/?username=${config.github.username}&style=for-the-badge)`,
-    ];
+    const websiteBadge = `<a href="${config.social.website}"><img src="https://img.shields.io/badge/-Website-${config.badges.colors.primary}?style=for-the-badge&logo=amp&logoColor=white" alt="Website Badge"/></a>`;
+    const linkedinBadge = `<a href="${config.social.linkedin}"><img src="https://img.shields.io/badge/-LinkedIn-${config.badges.colors.primary}?style=for-the-badge&logo=Linkedin&logoColor=white" alt="LinkedIn Badge"/></a>`;
+    const viewsBadge = `<img src="https://komarev.com/ghpvc/?username=${config.github.username}&style=for-the-badge" alt="Profile Views Count Badge"/>`;
 
-    return badges.join(" ");
-  }
-
-  private createBadge(name: string, url: string, logo = "amp"): string {
-    return `[![${name} Badge](https://img.shields.io/badge/-${name}-${config.badges.colors.primary}?style=for-the-badge&logo=${logo}&logoColor=white)](${url})`;
+    return `${websiteBadge} ${linkedinBadge} ${viewsBadge}`;
   }
 
   private generateGitHubStats(): string {
-    const options = {
-      username: config.github.username,
-      show_icons: true,
-      hide_border: true,
-      include_all_commits: true,
-      card_width: 600,
-      hide: "contribs",
-      show: "reviews,prs_merged,prs_merged_percentage",
-    };
+    const darkMode = `<a href="https://github.com/${config.github.username}/${config.github.username}#gh-dark-mode-only">
+      <img src="https://github-readme-stats.vercel.app/api?username=${config.github.username}&show_icons=true&hide_border=true&include_all_commits=true&card_width=600&custom_title=GitHub%20Open%20Source%20Stats&title_color=${config.badges.colors.primary}&text_color=${config.badges.colors.textDark}&icon_color=${config.badges.colors.primary}&hide=contribs&show=reviews,prs_merged,prs_merged_percentage&theme=transparent#gh-dark-mode-only" alt="GitHub Stats Dark Mode"/>
+    </a>`;
 
-    return [
-      this.createGitHubStatsCard(options, "dark"),
-      this.createGitHubStatsCard(options, "light"),
-    ].join("\n");
-  }
+    const lightMode = `<a href="https://github.com/${config.github.username}/${config.github.username}#gh-light-mode-only">
+      <img src="https://github-readme-stats.vercel.app/api?username=${config.github.username}&show_icons=true&hide_border=true&include_all_commits=true&card_width=600&custom_title=GitHub%20Open%20Source%20Stats&title_color=${config.badges.colors.primary}&text_color=${config.badges.colors.textLight}&icon_color=${config.badges.colors.primary}&hide=contribs&show=reviews,prs_merged,prs_merged_percentage&theme=transparent#gh-light-mode-only" alt="GitHub Stats Light Mode"/>
+    </a>`;
 
-  private createGitHubStatsCard(options: any, mode: "dark" | "light"): string {
-    const params = new URLSearchParams({
-      username: options.username,
-      show_icons: "true",
-      hide_border: "true",
-      include_all_commits: "true",
-      card_width: "600",
-      custom_title: "GitHub Open Source Stats",
-      title_color: config.badges.colors.primary,
-      text_color:
-        mode === "dark"
-          ? config.badges.colors.textDark
-          : config.badges.colors.textLight,
-      icon_color: config.badges.colors.primary,
-      hide: options.hide,
-      show: options.show,
-      theme: "transparent",
-    });
-
-    const url = `https://github-readme-stats.vercel.app/api?${params.toString()}#gh-${mode}-mode-only`;
-    const imageAlt = `GitHub-Stats-Card-${
-      mode.charAt(0).toUpperCase() + mode.slice(1)
-    }`;
-    return `[![${imageAlt}](${url})](https://github.com/${options.username}/${options.username}#gh-${mode}-mode-only)`;
+    return `${darkMode}\n${lightMode}`;
   }
 
   async generateReadme(reposList: string): Promise<string> {
-    const template = `
-<div align="center">
-  ${this.generateBadges()}
-  ---
-  Hi there 👋!
-  ---
-  ${this.generateGitHubStats()}
+    const template = `<div align="center">
+
+${this.generateBadges()}
+
+---
+
+Hi there 👋!
+
+---
+
+${this.generateGitHubStats()}
+
 </div>
 
 ---
@@ -86,9 +55,13 @@ ${reposList}
 
 ---
 
-<a href="https://gelbhart.dev" target="_blank" rel="noopener noreferrer">
-  <img src="https://gelbhart.dev/favicon.ico" width="30" align="center" />
-</a>`;
+<div align="center">
+<a href="${config.social.website}" target="_blank" rel="noopener noreferrer">
+  <img src="${
+    config.social.website
+  }/favicon.ico" width="30" alt="Website Icon"/>
+</a>
+</div>`;
 
     return this.md.render(template);
   }
